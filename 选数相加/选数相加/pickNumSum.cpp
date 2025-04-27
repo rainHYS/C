46404827c544318384e09524e9da255799633043
rainHYS <h994257519@gmail.com>
2025-04-27 20:38:30 +0800

    完成了选数相加的递归解决

--- 选数相加/选数相加/pickNumSum.cpp
+++ 选数相加/选数相加/pickNumSum.cpp
@@ -19,13 +19,38 @@ using namespace std;
 // 输出格式
 // 输出一个整数，表示种类数。
 
-void recursionSum(int current, int maxN, int maxK, int num[], bool status[]) {
-	if (current == maxN) {
+void recursionSum(int current, int maxN, int maxK, int num[], int result[], bool status[]) {
+	int sum = 0;
+	if (current > maxK) {
+		for (int i = 1; i <= maxK; i++) {
+			sum = sum + result[i];
+		}
+		bool check = true;
+		for (int i = 2; i < sum; i++) {
+			if (!(sum % i)) {
+				check = false;
+				break;
+			}
+		}
+		if (check) {
+			for (int j = 1; j <= maxK; j++) {
+				cout << result[j];
+				if (j < maxK) {
+					cout << "+";
+				}
+			}
+			cout << "=" << sum;
+			cout << endl;
+		}
 		return;
 	}
-	for (int i = 0; i < maxN; i++) {
-		if (!status[i]) {
+	for (int i = 1; i <= maxN; i++) {
+		if (!status[i] && num[i] > result[current - 1]) {
 			status[i] = true;
+			result[current] = num[i];
+			recursionSum(current + 1, maxN, maxK, num, result, status);
+			status[i] = false;
+			result[current] = 0;
 		}
 	}
 }
@@ -33,10 +58,11 @@ void recursionSum(int current, int maxN, int maxK, int num[], bool status[]) {
 int main() {
 	int n, k;
 	cin >> n >> k;
-	int* num = new int[n];
-	bool* status = new bool[n];
-	for (int i = 0; i < n; i++) {
+	int* num = new int[n + 1]();
+	int* result = new int[k + 1]();
+	bool* status = new bool[n + 1]();
+	for (int i = 1; i <= n; i++) {
 		cin >> num[i];
 	}
-	recursionSum(0, n, k, num, status);
+	recursionSum(1, n, k, num, result, status);
 }
