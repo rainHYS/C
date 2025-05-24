0a7a9c37d26bf683cdd4f1e1f2c1e8be9c29d7df
rainHYS <h994257519@gmail.com>
2025-05-24 20:59:56 +0800

    傻逼接龙，要求最小重叠部分

--- 洛谷C++/P1019 [NOIP 2000 提高组] 单词接龙/WordSolitaire.cpp
+++ 洛谷C++/P1019 [NOIP 2000 提高组] 单词接龙/WordSolitaire.cpp
@@ -1,14 +1,56 @@
-#include <iostream>
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
 int n;
-string* word;
+char start;
+string* words;
+int* used;
+int** overlap;
+int res = 0;
+
+void recWord(string dragon, int index) {
+	res = max(res, (int)dragon.size());
+	used[index]++;
+	for (int i = 0; i < n; i++) {
+		if (overlap[index][i] && used[i] < 2) {
+			recWord(dragon + words[i].substr(overlap[index][i]), i);
+		}
+	}
+	used[index]--;
+}
 
 int main() {
 	cin >> n;
-	word = new string[n+1]();
-	for (int i = 1; i <= n; i++) {
-		cin >> word[i];
+	words = new string[n];
+	for (int i = 0; i < n; i++) {
+		cin >> words[i];
 	}
-	cin >> word[0];
+	used = new int[n]();
+	overlap = new int* [n]();
+	for (int i = 0; i < n; i++) {
+		overlap[i] = new int[n]();
+	}
+
+	cin >> start;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			string suffix = words[i];
+			string prefix = words[j];
+			for (int k = 1; k < min(suffix.size(), prefix.size()); k++) {
+				if (suffix.substr(suffix.size() - k, k) == prefix.substr(0, k)) {
+					overlap[i][j] = k;
+					break;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (words[i][0] == start) {
+			recWord(words[i], i);
+		}
+	}
+	cout << res;
 }
